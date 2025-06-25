3aaa8aafd122207f012908ff80c4cff260119368
Shubham Ubale <ubalekartik203@gmail.com>
2025-06-25 10:14:08 +0530

    Add calender sheadule

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ from app.routers import crop
 from app.routers import mandi
 from app.routers import auth
 from app.routers import question
-from app.routers import locator
 from app.routers import weather
-from app.routers import notifications
-from app.routers import tools
 from app.routers import schemes
 from app.routers import recommendation_engine
+from app.routers import calendar
 
 
-app = FastAPI(title="Crop Disease Detection via Groq")
+app = FastAPI(title="Krushi AI")
 # app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(crop.router, prefix="/crop", tags=["Crop Diagnosis"])
 app.include_router(mandi.router)
@@ -19,6 +17,7 @@ app.include_router(weather.router)
 app.include_router(recommendation_engine.router)
 app.include_router(schemes.router)
 app.include_router(question.router, prefix="/question", tags=["Expert Q&A"])
+app.include_router(calendar.router)
 
 @app.get("/")
 def root():
