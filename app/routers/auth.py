f45c864f1e75b2d3805167ee92ac48e3e0ab7702
Shubh1619 <ubalekartik203@gmail.com>
2025-07-17 00:30:19 +0530

    login user!=get user2

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -32,6 +32,6 @@ async def show_reset_form(request: Request, token: str):
 async def reset_password(payload: ResetPasswordPayload):
     return auth_service.reset_password(payload)
 
-@router.get("/all-users", summary="Get all registered users")
-def all_users():
-    return auth_service.get_all_users()
+@router.get("/other-users/{current_user_id}", summary="Get all users except current user")
+def get_other_users(current_user_id: int):
+    return auth_service.get_all_users(current_user_id)
